d0bea565ee5d74919ffb9ece6604192bb83d1410
Cailie Skelton <cskelton707@gmail.com>
2020-07-22 00:17:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,16 +3,8 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     return "#{array[0]} and #{array[1]}"
- elsif array.length >    
-end
-
-def oxford_comma(array)
-  if array.length == 1 # hello
-    array.join
-  elsif array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif array.length > 2
-    array[-1].insert(0, "and ")
-    return array.join(", ")
+ elsif array.length > 2 
+  array[-1].insert(0, "and")
+  return array.join(", ")
  end
 end
